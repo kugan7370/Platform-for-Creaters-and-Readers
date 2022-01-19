8e41027066d85c544373be61359d911e54f2b4c8
Kugan Priyan <88609808+kugan7370@users.noreply.github.com>
2022-01-20 03:35:53 +0530

    ADD Blog

--- Components/Blog/BlogHeader.js
+++ Components/Blog/BlogHeader.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
-import { auth } from '../../Firebase';
+import { auth, db } from '../../Firebase';
 import { signOut } from 'firebase/auth';
+import { collection, doc, getDocs, onSnapshot, query, where } from 'firebase/firestore';
 
+const profile = 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg';
 
 export default function BlogHeader() {
     const navigation = useNavigation();
+    const [user, setuser] = useState(null);
+
+
+    const getuser = async () => {
+
+        const q = query(collection(db, 'users'), where('uid', '==', auth.currentUser.uid));
+        const docSnap = await getDocs(q);
+
+        docSnap.docs.map((doc) => {
+
+            setuser({
+
+                pro_pic: doc.data().pro_pic,
+                username: doc.data().username,
+            })
+
+        })
+
+    }
+
+
+    useEffect(() => {
+        getuser();
+    }, [])
+
 
 
     const userSignOut = () => {
@@ -23,13 +50,13 @@ export default function BlogHeader() {
         <View style={style.container}>
             <SafeAreaView style={{ backgroundColor: '#f7f7f7', height: 80 }}>
                 <View style={style.headContainer}>
-                    <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+                    <TouchableOpacity onPress={() => navigation.navigate('AddBlog')}>
                         <Ionicons name="menu" size={30} color="black" />
                     </TouchableOpacity>
 
                     <Text style={style.headText}>Blogs</Text>
                     <TouchableOpacity onPress={userSignOut} style={style.imageContainer}>
-                        <Image style={style.image} source={{ uri: 'https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg' }} />
+                        <Image style={style.image} source={{ uri: user ? user.pro_pic : profile }} />
                     </TouchableOpacity>
 
                 </View>
@@ -65,6 +92,5 @@ const style = StyleSheet.create({
         width: '100%',
         borderRadius: 30,
         resizeMode: 'cover'
-
     }
 })
